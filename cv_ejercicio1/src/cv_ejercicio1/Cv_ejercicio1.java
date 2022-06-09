416b310d9a189ed90ad055a27858e4a1ef011dbe
Usuario <Usuario@Usuario-PC>
2022-06-09 15:30:41 -0500

    alexander_vallejo11

---
+++ cv_ejercicio1/src/cv_ejercicio1/Cv_ejercicio1.java
@@ -0,0 +1,23 @@
+
+package cv_ejercicio1;
+
+/**
+ *
+ * @author Alexander Vallejo Lara.
+ */
+public class Cv_ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("versión 0");
+        
+        
+        
+    }
+    
+    
+}
